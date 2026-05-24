f98940a63d1a62efc6d34fdce81dbe20d30bdfee
John <guineyjp@gmail.com>
2026-05-24 21:34:34 +0100

    created grid, allows users to change number of grids, hover effect

---
+++ script.js
@@ -0,0 +1,56 @@
+for (i = 0; i < 256; i++) {
+    //creating a square element
+
+const square = document.createElement("div");
+
+square.classList.add("square");
+
+//append square as a child inside container div
+document.getElementById("container").appendChild(square);
+
+square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "blue";
+    
+     })
+
+
+}
+
+//select resize button from DOM
+const button = document.querySelector("#resize-btn");
+
+button.addEventListener("click", () => {
+
+    let userInput = prompt("Enter number of squares (maximum 100)");
+    document.getElementById("container").innerHTML ="";
+
+    //creating an if statement to limit user input to 100
+
+    if (userInput > 100) {
+         alert("Sorry, you must enter a value of 100 or less.")
+    } else {
+    for (i = 0; i < userInput * userInput; i++) {
+    //creating a square element
+
+const square = document.createElement("div");
+
+square.classList.add("square");
+
+/*the size of each box should be defined by the total number of squares and the overall size of the page
+Size of the page is fixed at 960px width and 960px height,*/
+
+square.style.width = (960 / userInput) + "px";
+square.style.height = (960 / userInput) + "px";
+
+//append square as a child inside container div
+document.getElementById("container").appendChild(square);
+
+square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "blue";}
+)
+    
+     }
+
+    }
+})
+
